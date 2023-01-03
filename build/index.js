1713d0f3267972b20e6bf67e516ecadce211bf2a
Murilo <murilodesousag@gmail.com>
2023-01-03 15:50:49 -0300

    finalizar ex3

--- build/index.js
+++ build/index.js
@@ -3,4 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
 console.log("usuarios");
 console.table(database_1.users);
+console.log("Produtos");
+console.table(database_1.product);
+console.log("Carinho");
+console.table(database_1.purchease);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import {users} from "./database"
+import {product, purchease, users} from "./database"
 
 console.log("usuarios")
 console.table(users)
+console.log("Produtos")
+console.table(product)
+console.log("Carinho")
+console.table(purchease)

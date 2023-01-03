3c16c7bbe352927dc36842989a53ad4e22c38cd9
Murilo <murilodesousag@gmail.com>
2023-01-03 15:43:34 -0300

    edit gitignore 2

--- build/index.js
+++ build/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
-console.log("funcionou");
 console.log("usuarios");
 console.table(database_1.users);
 //# sourceMappingURL=index.js.map
